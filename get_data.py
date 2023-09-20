7fff84409c6e83913a19b52dfee7bbb8115c0315
unknown <andreasheggelund@gmail.com>
2023-09-20 23:41:07 +0200

    Added inital data fetch

--- get_data.py
+++ get_data.py
@@ -1,1 +1,28 @@
-import matplotlib.pyplot as plt
+import os
+import requests
+import pandas as pd
+
+def get_source_ids(municipalities = ['Hamar']):
+    
+    endpoint_sources = 'https://frost.met.no/sources/v0.jsonld'
+    
+    for i, municipality in enumerate(municipalities):
+        
+        source_parameters = {
+            'municipality' : municipalities[i],
+        }
+        
+        r = requests.get(endpoint_sources, source_parameters, auth=(os.getenv('client_id'),''))
+        
+        source_response = r.json()
+
+        for datapoint in source_response['data']:
+            print(datapoint['name'], datapoint['id'])
+    
+
+municipalities = ['Hamar', 'Ringsaker', 'Stange']
+
+get_source_ids(municipalities)
+
+
+
