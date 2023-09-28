48c7d16bdc740b4261ac1098b41034ecbaf8c285
a01798 <andreas-lokken.heggelund@norsk-tipping.no>
2023-09-28 08:59:40 +0200

    added function to retrieve radar images

--- get_data.py
+++ get_data.py
@@ -1,9 +1,16 @@
 import os
 import requests
 import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
+
+
+
 
 def get_source_ids(municipalities = ['Hamar']):
-    
+    """Takes a list of municipalities as input, returns a list of source IDs for every measuring point in that municipality"""
+
     endpoint_sources = 'https://frost.met.no/sources/v0.jsonld'
     
     source_ids = []
@@ -25,7 +32,8 @@ def get_source_ids(municipalities = ['Hamar']):
 
     return source_ids
 
-def get_sensor_data(observation_locations, measurement_types):
+def get_sensor_data(observation_locations = [], measurement_types = ""):
+    """Takes a list of measurement stations IDs and a string of measurement types as input, returns all measurement data from given locations and measurement types as a single dataframe."""
 
     endpoint_observations = 'https://frost.met.no/observations/v0.jsonld'
 
@@ -53,12 +61,32 @@ def get_sensor_data(observation_locations, measurement_types):
             df = pd.concat([df, single_observation])
             
     print(df.head(50))
+    return df
 
+def get_image_data(type, region, month, day, hour):
+
+    url = 'https://api.met.no/weatherapi/radar/2.0/?type={}&area={}&time=2023-{}-{}T{}:00:00Z'.format(type, region, month, day, hour)
+    print('/n'+url)
+    radar_image = requests.get(url)
+    
+    with open("radar_image_{}_{}_2023{}{}-{}h.png".format(type, region, month, day, hour), "wb") as f:
+        for chunk in radar_image:
+            f.write(chunk)
+
+    #
+    #image = plt.imshow(radar_image)
+    #plt.show()
+    
+#while (datetime.now().minute > 00 and 
+currentDay = str(datetime.now().strftime('%d'))
+currentHour = str((datetime.now()- timedelta(hours=2)).strftime('%H'))
+currentMonth = datetime.now().strftime('%m')
+
+get_image_data('accumulated_01h','eastern_norway', currentMonth, currentDay, currentHour)
+
+"""
 municipalities = ['Hamar', 'Ringsaker', 'Stange']
 measurements = 'mean(air_temperature P1D),sum(precipitation_amount P1D),mean(wind_speed P1D)'
 observation_ids = get_source_ids(municipalities)
 get_sensor_data(observation_ids, measurements)
-
-
-
-#jhdfgkdj
+"""
