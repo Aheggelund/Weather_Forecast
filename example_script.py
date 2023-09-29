544a6fc8ce323628c99ce86182c020cfda4e1eb5
unknown <andreasheggelund@gmail.com>
2023-09-29 10:49:03 +0200

    Refactored get_data into class, added example_script that uses GetData()

---
+++ example_script.py
@@ -0,0 +1,17 @@
+import get_data
+from datetime import datetime, timedelta
+
+currentDay = str(datetime.now().strftime('%d'))
+currentHour = str((datetime.now()- timedelta(hours=2)).strftime('%H'))
+currentMonth = datetime.now().strftime('%m')
+
+savefile_path = r'C:/Users/Andreas/Desktop/radarImg/'
+gg = get_data.GetData()
+gg.get_image_data('accumulated_01h','eastern_norway', currentMonth, currentDay, currentHour, savefile_path)
+
+"""
+municipalities = ['Hamar', 'Ringsaker', 'Stange']
+measurements = 'mean(air_temperature P1D),sum(precipitation_amount P1D),mean(wind_speed P1D)'
+observation_ids = get_source_ids(municipalities)
+get_sensor_data(observation_ids, measurements)
+"""
